5f6a5b5d436c51edef9b1b5cec40192c3f9bbe00
Chris Javier Oliveros <chrisjavieroliveros@gmail.com>
2025-06-16 20:24:53 +0300

    refactor: Update header structure, theme URI, and color definitions for consistency
    - Commented out the header markup for potential future use, streamlining the layout.
    - Updated the theme URI in style.css for accurate branding.
    - Revised color values in _colors.scss for secondary and dark color variants to enhance visual coherence and accessibility.

--- header.php
+++ header.php
@@ -16,17 +16,15 @@
 	<meta charset="<?php bloginfo( 'charset' ); ?>">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 	<link rel="profile" href="https://gmpg.org/xfn/11">
-
 	<?php wp_head(); ?>
 </head>
 
 <body <?php body_class(); ?>>
 <?php wp_body_open(); ?>
 
-
 <div id="smooth-wrapper">
 	<div id="smooth-content">
 
-		<header id="masthead" class="site-header">
-			<?php get_template_part( 'template-parts/navigation/navigation' ); ?>
-		</header>
+		<!-- <header id="masthead" class="site-header">
+			<?php // get_template_part( 'template-parts/navigation/navigation' ); ?>
+		</header> -->
